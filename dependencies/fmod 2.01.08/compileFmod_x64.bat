4fe762d7071ab52c1f2a4b1b766a25bc25cd3316
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2021-03-22 15:08:25 +0100

    Fixed: Fmod bats (read info)
    Se han cambiado los valores de los eventos de post compilación de xcopy para que pasen a copiarse solo en caso de que los ficheros no existan o si encuentra una diferencia entre estos.
    
    Para el caso de las diferencias, contrasta las fechas de modificación, dejará en el repositorio las que tengan una fecha "más nueva", es decir, los que se hayan modificado en un tiempo más cercano al momento de la ejecución.

--- dependencies/fmod 2.01.08/compileFmod_x64.bat
+++ dependencies/fmod 2.01.08/compileFmod_x64.bat
@@ -1,7 +1,7 @@
-rem copia las dlls implicitas a bin
-xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmod.dll ..\..\bin\ /y
-xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmod_vc.lib ..\..\bin\ /y
-xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmodL.dll ..\..\bin\ /y
-xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmodL_vc.lib ..\..\bin\ /y
+rem copia las dlls y libs en caso de que sean nuevas -noexistan- o se hayan modificado
+xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmod.dll ..\..\bin\ /s /d
+xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmod_vc.lib ..\..\bin\ /s /d
+xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmodL.dll ..\..\bin\ /s /d
+xcopy ..\..\dependencies\fmod 2.01.08\Buildx64\fmodL_vc.lib ..\..\bin\ /s /d
 
 exit 0
