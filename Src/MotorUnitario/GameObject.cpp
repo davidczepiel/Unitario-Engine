87f7adbc35d7a3b60651cc80d08b9212bb1bb5fb
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-03-26 12:42:59 +0100

    FEAT: avance en gameobject y quitado Error

--- Src/MotorUnitario/GameObject.cpp
+++ Src/MotorUnitario/GameObject.cpp
@@ -1,7 +1,8 @@
 #include "GameObject.h"
 #include "Component.h"
+#include "Exceptions.h"
 
-GameObject::GameObject() : children(), components(), transform(nullptr), parent(nullptr), enable(true), persist(false)
+GameObject::GameObject() : _children(), _components(), _parent(nullptr), _enable(true), _persist(false)
 {
 	//transform = new Transform();
 	//components->add(0, tranform);		???
@@ -9,60 +10,78 @@ GameObject::GameObject() : children(), components(), transform(nullptr), parent(
 
 void GameObject::start()
 {
-	for (Component* comp : components)
+	for (Component* comp : _components)
 		if(comp->getEnable()) 
 			comp->start();
 
-	for (GameObject* go : children)
+	for (GameObject* go : _children)
 		if(go->getEnable()) 
 			go->start();
 }
 
 void GameObject::update()
 {
-	for (Component* comp : components)
+	for (Component* comp : _components)
 		if (comp->getEnable()) 
 			comp->update();
 
-	for (GameObject* go : children)
+	for (GameObject* go : _children)
 		if (go->getEnable()) 
 			go->update();
 }
 
 void GameObject::fixedUpdate()
 {
-	for (Component* comp : components)
+	for (Component* comp : _components)
 		if (comp->getEnable()) 
 			comp->fixedUpdate();
 
-	for (GameObject* go : children)
+	for (GameObject* go : _children)
 		if (go->getEnable()) 
 			go->fixedUpdate();
 }
 
 void GameObject::lateUpdate()
 {
-	for (Component* comp : components)
+	for (Component* comp : _components)
 		if (comp->getEnable()) 
 			comp->fixedUpdate();
 
-	for (GameObject* go : children)
+	for (GameObject* go : _children)
 		if (go->getEnable()) 
 			go->lateUpdate();
 }
 
-void GameObject::addComponent(Component* component_)
+void GameObject::addComponent(Component* component)
 {
+	int id = component->getId();
 
+	if (id < 0 || id > _components.size())
+		throw "Id of component doesnt correspond to any component";
+
+	_components[id] = component;
 }
 
-void GameObject::removeComponent(Component* component_)
+void GameObject::removeComponent(unsigned int componentId)
 {
+	if (componentId < 0 || componentId > _components.size())
+		throw "Id of component doesnt correspond to any component";
 
+	delete _components[componentId];
+	_components[componentId] = nullptr;
 }
 
-void GameObject::addChild(GameObject* gameObject_)
+
+void GameObject::addChild(GameObject* gameObject)
 {
-	gameObject_->setParent(this);
-	children.push_back(gameObject_);
+	gameObject->setParent(this);
+	_children.push_back(gameObject);
+}
+
+inline Component* GameObject::getComponent(unsigned int componentId) const
+{
+	if (componentId < 0 || componentId > _components.size())
+		throw "Id of component doesnt correspond to any component";
+
+	return _components[componentId];
 }
--- Src/MotorUnitario/GameObject.h
+++ Src/MotorUnitario/GameObject.h
@@ -1,5 +1,10 @@
 #pragma once
+
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
 #include <list>
+#include <vector>
 
 class Transform;
 class Component;
@@ -30,61 +35,72 @@ public:
 	/// </summary>
 	void lateUpdate();
 
-	//No se si el componente tendria un "id" para asignarselo al vector o al diccionario
-	void addComponent(Component* component_);
+	/// <summary>
+	/// Add a full component to the gameObject vector of component
+	/// </summary>
+	void addComponent(Component* component);
 	
-	//Aqui estaria mejor recibir un "id"
-	void removeComponent(Component* component_);
+	/// <summary>
+	/// Removes component from gameObject
+	/// </summary>
+	/// <param name="componentId_">: id of the component to remove</param>
+	/// <exception cref="Component.Exception">Thrown when componentId doesnt correspond to any component</exception>
+	void removeComponent(unsigned int componentId);
 
 	/// <summary>
 	/// Adds gameObject as a child of this gameObject. 
 	/// <para> Parent of the child is set to this gameObject. </para>
 	/// </summary>
 	/// <param name="gameObject_">: child</param>
-	void addChild(GameObject* gameObject_);
+	void addChild(GameObject* gameObject);
 
-	//Aqui estaria mejor recibir un "id"
-	inline Component* getComponent(Component* component_) const {
-		return nullptr;
-	}
+	/// <summary>
+	/// Returns a component of the gameObject
+	/// </summary>
+	/// <param name="componentId_"></param>
+	/// <exception cref="Component.Exception">Thrown when componentId doesnt correspond to any component</exception>
+	/// <returns>Component* if it exists, nullptr if it doesn't</returns>
+	inline Component* getComponent(unsigned int componentId) const;
 
 	inline const std::list<GameObject*>& getChildren() const {
-		return children;
+		return _children;
 	}
 
 	inline const GameObject* getParent() const {
-		return parent;
+		return _parent;
 	}
 
-	inline void setParent(GameObject* gameObject_) {
-		parent = gameObject_;
+	inline void setParent(GameObject* gameObject) {
+		_parent = gameObject;
 	}
 
 	inline const bool getEnable() const {
-		return enable;
+		return _enable;
 	}
 
-	inline void setEnable(bool enable_) {
-		enable = enable_;
+	inline void setEnable(bool enable) {
+		_enable = enable;
 	}
 
 	inline const bool getPersist() const {
-		return persist;
+		return _persist;
 	}
 
-	inline void setPersist(bool persist_) {
-		persist = persist_;
+	inline void setPersist(bool persist) {
+		_persist = persist;
 	}
 
 private:
-	std::list<GameObject*> children;
+	std::list<GameObject*> _children;
 
 	//Si se usa un id, la lista de componentes podria pasar a un vector, o un diccionario (un vector seria mas rapido)
 	//Si se usa un id, el component Transform se podria meter junto a los otros componentes, con id = 0 por ejemplo
-	std::list<Component*> components;
-	Transform* transform;
+	std::vector<Component*> _components;
 
-	GameObject* parent;
+	GameObject* _parent;
 
-	bool enable, persist;
+	bool _enable, _persist;
 };
+
+
+#endif GAMEOBJECT_H
