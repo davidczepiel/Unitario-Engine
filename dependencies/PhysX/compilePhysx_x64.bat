45fea8346b3ed2d1cc1ad730a9c815df2fef9ea8
Sandra <samondra@ucm.es>
2021-04-05 15:54:02 +0200

    FIX: .bat separados en DEBUG y RELEASE
    Debido a que los archivos tienen los mismos nombres para debug y release se deben de hacer 4 .bat
    
    Co-Authored-By: nicoFdez <47755232+nicoFdez@users.noreply.github.com>
    Co-Authored-By: Alonefcp <47754529+Alonefcp@users.noreply.github.com>

--- dependencies/PhysX/compilePhysx_x64.bat
+++ dependencies/PhysX/compilePhysx_x64.bat
@@ -1,12 +1,4 @@
 rem copia las dlls en caso de que sean nuevas -noexistan- o se hayan modificado. Version inicial 2.01.08
-rem DEBUGx64
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\glut32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysX_64.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysXCommon_64.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysXCooking_64.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysXDevice64.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysXFoundation_64.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx64\debug\PhysXGpu_64.dll ..\..\bin\ /s /d /y
 rem RELEASEx64
 xcopy ..\..\dependencies\PhysX\Buildx64\release\glut32.dll ..\..\bin\ /s /d /y
 xcopy ..\..\dependencies\PhysX\Buildx64\release\PhysX_64.dll ..\..\bin\ /s /d /y
--- dependencies/PhysX/compilePhysx_x86.bat
+++ dependencies/PhysX/compilePhysx_x86.bat
@@ -1,13 +1,5 @@
 rem copia las dlls en caso de que sean nuevas -noexistan- o se hayan modificado. Version inicial 2.01.08
 rem DEBUGx86
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\glut32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysX_32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysXCommon_32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysXCooking_32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysXDevice.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysXFoundation_32.dll ..\..\bin\ /s /d /y
-xcopy ..\..\dependencies\PhysX\Buildx86\debug\PhysXGpu_32.dll ..\..\bin\ /s /d /y
-rem RELEASEx86
 xcopy ..\..\dependencies\PhysX\Buildx86\release\glut32.dll ..\..\bin\ /s /d /y
 xcopy ..\..\dependencies\PhysX\Buildx86\release\PhysX_32.dll ..\..\bin\ /s /d /y
 xcopy ..\..\dependencies\PhysX\Buildx86\release\PhysXCommon_32.dll ..\..\bin\ /s /d /y
