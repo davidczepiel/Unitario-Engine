202c7c8ffab74c256dab13ab5986ec4d8dd06a40
Zhefan <miguelye@ucm.es>
2021-04-06 22:49:59 +0200

    FEAT: Lectura de archivo.lua
    Es una simple suma

--- Src/MotorUnitario/main.cpp
+++ Src/MotorUnitario/main.cpp
@@ -21,19 +21,24 @@ WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdS
 	lua_State* L = luaL_newstate();
 
 	int r = luaL_dostring(L, test.c_str());
-
+	auto aux = luaL_dofile(L, "Assets/Levels/test.lua");
 	if (r == LUA_OK) {
 		lua_getglobal(L, "o");
 		if (lua_isnumber(L, -1)) {
 			int a = (int)lua_tonumber(L, -1);
-			std::cout << a << std::endl;
-
+			std::cout << "suma guay " << a << std::endl;
 		}
 	}
 	else
 	{
 		std::cout << lua_tostring(L, -1) << std::endl;
 	}
+
+	if (aux == LUA_OK) {
+		int b = (int)lua_tonumber(L, -1);
+		std::cout << "archivo guay " << b << std::endl;
+	}
+	else std::cout << "error archivo" << std::endl;
 
 	lua_close(L);
 	Engine* prueba = Engine::getInstance();
