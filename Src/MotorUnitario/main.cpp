46688211959de845435a323781885e73d037b27e
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-04-06 09:43:59 +0200

    FIX: limpieza main

--- Src/MotorUnitario/main.cpp
+++ Src/MotorUnitario/main.cpp
@@ -1,9 +1,5 @@
 #define SDL_MAIN_HANDLED
-#include "MotorGrafico/main.h"
-#include "MotorAudio/main.h"
-#include "MotorFisico/main.h"
-#include <SDL.h>
-#include <string>
+
 #include "Engine.h"
 
 #if (defined _DEBUG) || !(defined _WIN32)
@@ -14,10 +10,9 @@ int WINAPI
 WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
 #endif
 
-	pruebaPx();
 	Engine* prueba = Engine::getInstance();
 	prueba->init();
 	prueba->run();
-	//Sleep(1000);
+
 	return 0;
 }
