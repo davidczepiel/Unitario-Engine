a6dc61b7f6da7d3cee7f9d542a611c674f081d24
Zhefan <miguelye@ucm.es>
2021-04-06 21:05:59 +0200

    FEAT: Lua & .bat implementados (tambien el .bat para eliminar dlls )
    Co-Authored-By: MarcoIMH <marcoime@ucm.es>

--- Src/MotorUnitario/main.cpp
+++ Src/MotorUnitario/main.cpp
@@ -1,18 +1,45 @@
 #define SDL_MAIN_HANDLED
 
 #include "Engine.h"
+#include <iostream>
+extern "C"
+{
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
 
 #if (defined _DEBUG) || !(defined _WIN32)
+#pragma comment (lib, "liblua.a")
 int main() {
 #else
 #include <Windows.h>
 int WINAPI
 WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
 #endif
+	std::string test = "o = 4 + 5";
+	lua_State* L = luaL_newstate();
 
+	int r = luaL_dostring(L, test.c_str());
+
+	if (r == LUA_OK) {
+		lua_getglobal(L, "o");
+		if (lua_isnumber(L, -1)) {
+			int a = (int)lua_tonumber(L, -1);
+			std::cout << a << std::endl;
+
+		}
+	}
+	else
+	{
+		std::cout << lua_tostring(L, -1) << std::endl;
+	}
+
+	lua_close(L);
 	Engine* prueba = Engine::getInstance();
 	prueba->init();
 	prueba->run();
+	//prueba->stopExecution();
 
 	return 0;
 }
---
+++ dependencies/Lua/compileLua_x86.bat
@@ -0,0 +1,4 @@
+rem copia las dlls en caso de que sean nuevas -noexistan- o se hayan modificado. Version inicial 2.01.08
+xcopy ..\..\dependencies\Lua\Buildx86\lua.dll ..\..\bin\ /s /d /y
+
+exit 0
