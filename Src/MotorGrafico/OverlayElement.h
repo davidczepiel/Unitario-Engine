c7fc9d3b7fddde46a22850316569ef4a269dc065
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2021-04-18 11:54:52 +0200

    UPDATE: Clase OverlayElement comentada

--- Src/MotorGrafico/OverlayElement.h
+++ Src/MotorGrafico/OverlayElement.h
@@ -8,14 +8,35 @@ namespace Ogre {
 
 class OverlayElement {
 public:
+	/// <summary>
+	/// Class constructor
+	/// </summary>
+	/// <param name="overlayName"> Overlay name (in .overlay)</param>
 	OverlayElement(std::string const& overlayName);
 	~OverlayElement();
 
+	/// <summary>
+	/// Shows the overlay
+	/// </summary>
 	void showOverlay();
+
+	/// <summary>
+	/// Hides the overlay
+	/// </summary>
 	void hideOverlay();
+
+	/// <summary>
+	/// Sets the material of an overlays child
+	/// </summary>
+	/// <param name="containerName"> Name of the overlays child </param>
+	/// <param name="materialName"> New material name </param>
 	void setMaterial(std::string const& containerName, std::string const& materialName);
 protected:
 	Ogre::Overlay* _overlay;
 private:
+	/// <summary>
+	/// private method to load an overlay from a .overlay file
+	/// </summary>
+	/// <param name="overlayName"></param>
 	void loadOverlay(std::string const& overlayName);
 };
