7f11a269d6bf618c1b48e77497737405c36d94b8
Alonefcp <fcuadra@ucm.es>
2021-04-20 15:40:33 +0200

    UPDATE ImageRenderComponent: añadido metodo start en el ImageRenderComponent
    Añadido metodo start que coge el Transform  y establece la posicion del componente

--- Src/MotorUnitario/ImageRenderComponent.cpp
+++ Src/MotorUnitario/ImageRenderComponent.cpp
@@ -1,17 +1,30 @@
 #include "ImageRenderComponent.h"
 #include "ComponentIDs.h"
+#include "Transform.h"
+#include "GameObject.h"
 
-ImageRenderComponent::ImageRenderComponent() : Component(ComponentId::ImageRender), imageRender(nullptr)
+ImageRenderComponent::ImageRenderComponent() : Component(ComponentId::ImageRender), _imageRender(nullptr),_tr(nullptr)
 {
-	imageRender = new ImageRender();
+	_imageRender = new ImageRender();
 }
 
-ImageRenderComponent::ImageRenderComponent(GameObject* gameObject) :Component(ComponentId::ImageRender, gameObject), imageRender(nullptr)
+ImageRenderComponent::ImageRenderComponent(GameObject* gameObject) :Component(ComponentId::ImageRender, gameObject), _imageRender(nullptr),_tr(nullptr)
 {
-	imageRender = new ImageRender();
+	_imageRender = new ImageRender();
 }
 
 ImageRenderComponent::~ImageRenderComponent()
 {
-	delete imageRender; imageRender == nullptr;
+	delete _imageRender; _imageRender == nullptr;
+	delete _tr; _tr = nullptr;
+}
+
+void ImageRenderComponent::start()
+{
+	_tr = static_cast<Transform*>(_gameObject->getComponent(ComponentId::Transform));
+	float x = static_cast<float>(_tr->getPosition().getX());
+	float y = static_cast<float>(_tr->getPosition().getY());
+	float z = static_cast<float>(_tr->getPosition().getZ());
+
+	_imageRender->setPosition(x, y, z);
 }
