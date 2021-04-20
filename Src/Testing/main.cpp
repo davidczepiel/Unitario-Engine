189e40aae34c5a884d84af5acbb91efd1ad98dca
Zhefan <miguelye@ucm.es>
2021-04-20 11:07:52 +0200

    FEAT: Luaparser in testing
    No carga archivos

--- Src/Testing/main.cpp
+++ Src/Testing/main.cpp
@@ -1,6 +1,7 @@
 #include "MotorUnitario/Engine.h"
 #include "MotorUnitario/Exceptions.h"
 #include "MotorUnitario/Logger.h"
+#include"MotorUnitario/LuaParser.h"
 
 #if (defined _DEBUG) || !(defined _WIN32)
 int main() {
@@ -11,6 +12,7 @@ WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdS
 #endif
 
 	try {
+		LuaParser* l = new LuaParser();
 		Engine* prueba = Engine::getInstance();
 		prueba->init();
 		prueba->run();
