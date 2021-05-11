105f12d8f7b342c636a5e0e508f1764cdbd5b29b
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2021-05-11 16:35:50 +0200

    FEAT: Interfaz de motorUnitario para controlar textos de la GUI

---
+++ Src/MotorUnitario/TextManagerElement.cpp
@@ -0,0 +1,51 @@
+#include "TextManagerElement.h"
+#include "MotorGrafico/OgreText.h"
+
+TextManagerElement::TextManagerElement(std::string overlayName, std::string textAreaName)
+{
+	_ogreText = new OgreText(overlayName, textAreaName);
+}
+
+TextManagerElement::~TextManagerElement()
+{
+}
+
+void TextManagerElement::setText(std::string szString)
+{
+	_ogreText->setText(szString);
+}
+
+void TextManagerElement::setPosition(float x, float y)
+{
+	_ogreText->setPosition(x, y);
+}
+
+void TextManagerElement::setColour(float R, float G, float B, float A)
+{
+	_ogreText->setColour(R, G, B, A);
+}
+
+void TextManagerElement::setCharHeight(float h)
+{
+	_ogreText->setCharHeight(h);
+}
+
+void TextManagerElement::setAlignment(int aligmentType)
+{
+	_ogreText->setAlignment(aligmentType);
+}
+
+void TextManagerElement::setEnabled(bool e)
+{
+	_ogreText->setEnabled(e);
+}
+
+void TextManagerElement::setFontName(std::string fontName)
+{
+	_ogreText->setFontName(fontName);
+}
+
+void TextManagerElement::setDimensions(float w, float h)
+{
+	_ogreText->setDimensions(w, h);
+}
