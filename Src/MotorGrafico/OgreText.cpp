ccc41f0cc0efa2b85ee4f67f538bf29d3212668c
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2021-05-11 16:11:52 +0200

    UPDATE: OgreText clase ampliada

--- Src/MotorGrafico/OgreText.cpp
+++ Src/MotorGrafico/OgreText.cpp
@@ -15,18 +15,52 @@ OgreText::~OgreText()
 void OgreText::setText(std::string szString)
 {
     _textArea->setCaption(szString);
-    //_textArea->setDimensions(1.0f, 1.0f);
-    //_textArea->setMetricsMode(Ogre::GMM_RELATIVE);
-    //_textArea->setFontName("Arial");
-    //_textArea->setCharHeight(0.03f);
 }
 
-void OgreText::setPos(float x, float y)
+void OgreText::setPosition(float x, float y)
 {
     _textArea->setPosition(x, y);
 }
 
-void OgreText::setCol(float R, float G, float B, float I)
+void OgreText::setColour(float R, float G, float B, float I)
 {
     _textArea->setColour(Ogre::ColourValue(R, G, B, I));
+}
+
+void OgreText::setCharHeight(float h)
+{
+    _textArea->setCharHeight(h);
+}
+
+void OgreText::setAlignment(int aligmentType)
+{
+    switch (aligmentType)
+    {
+    case 1:
+        _textArea->setAlignment(Ogre::TextAreaOverlayElement::Alignment::Left);
+        break;
+    case 2:
+        _textArea->setAlignment(Ogre::TextAreaOverlayElement::Alignment::Center);
+        break;
+    case 3:
+        _textArea->setAlignment(Ogre::TextAreaOverlayElement::Alignment::Right);
+        break;
+    default:
+        break;
+    }
+}
+
+void OgreText::setEnabled(bool e)
+{
+    _textArea->setEnabled(e);
+}
+
+void OgreText::setFontName(std::string fontName)
+{
+    _textArea->setFontName(fontName);
+}
+
+void OgreText::setDimensions(float w, float h)
+{
+    _textArea->setDimensions(w, h);
 }
--- Src/MotorGrafico/OgreText.h
+++ Src/MotorGrafico/OgreText.h
@@ -26,12 +26,42 @@ public:
     /// </summary>
     /// <param name="x"></param>
     /// <param name="y"></param>
-    void setPos(float x, float y);
+    void setPosition(float x, float y);
 
     /// <summary>
     /// Sets the text colour in RGBA
     /// </summary>
-    void setCol(float R, float G, float B, float I);
+    void setColour(float R, float G, float B, float I);
+
+    /// <summary>
+    /// Sets the text character height 
+    /// </summary>
+    /// <param name="h"></param>
+    void setCharHeight(float h);
+
+    /// <summary>
+    /// Sets the aligment of the text
+    /// </summary>
+    /// <param name="aligmentType">1 -> Left | 2 -> Center | 3 -> Right</param>
+    void setAlignment(int aligmentType);
+
+    /// <summary>
+    /// Enables or disables the text
+    /// </summary>
+    void setEnabled(bool e);
+
+    /// <summary>
+    /// Sets the font name of the text
+    /// </summary>
+    /// <param name="fontName"></param>
+    void setFontName(std::string fontName);
+
+    /// <summary>
+    /// Sets the dimension of this element in relation to the screen (1.0 = screen width/height)
+    /// </summary>
+    /// <param name="w">widht</param>
+    /// <param name="h">height</param>
+    void setDimensions(float w, float h);
 private:
     Ogre::OverlayManager* _overlayManager;
     Ogre::TextAreaOverlayElement* _textArea;
