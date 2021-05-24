796a5204f7f2a1ff837330d6700e1546fe624391
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-05-24 11:09:46 +0200

    FIX: LightComponent includes

--- Src/MotorUnitario/LightComponent.cpp
+++ Src/MotorUnitario/LightComponent.cpp
@@ -3,6 +3,7 @@
 #include "GameObject.h"
 #include "includeLUA.h"
 #include "Transform.h"
+#include "MotorGrafico/Light.h"
 
 LightComponent::LightComponent() :Component(ComponentId::LightComponent), _light(nullptr), _tr(nullptr)
 {
@@ -18,7 +19,7 @@ void LightComponent::awake(luabridge::LuaRef& data)
 	_light = new Light(_gameObject->getName());
 
 	if (LUAFIELDEXIST(LightType))
-		_light->setLightType(convertLightType(GETLUASTRINGFIELD(LightType)));
+		_light->setLightType((Light::LightType)convertLightType(GETLUASTRINGFIELD(LightType)));
 		//_light->setLightType(convertLightType(data["LightType"]));
 
 	if (LUAFIELDEXIST(Visible))
@@ -58,11 +59,116 @@ void LightComponent::update()
 	_light->setPosition(x, y, z);
 }
 
-const Light::LightType LightComponent::convertLightType(std::string type)
+void LightComponent::setType(LightComponent::LightType type)
 {
-	if (type == "POINT") return Light::LightType::POINT;
-	else if (type == "DIRECTIONAL") return Light::LightType::DIRECTIONAL;
-	else if (type == "SPOTLIGHT") return Light::LightType::SPOTLIGHT;
+	_light->setLightType((Light::LightType)type);
+}
+
+LightComponent::LightType LightComponent::getLightType()
+{
+	return (LightComponent::LightType)_light->getLightType();
+}
+
+void LightComponent::setDiffuse(float red, float green, float blue)
+{
+	_light->setDiffuse(red, green, blue);
+}
+
+void LightComponent::setDiffuse(const Colour& diffuse)
+{
+	_light->setDiffuse(diffuse.red, diffuse.green, diffuse.blue);
+}
+
+const Colour& LightComponent::getDiffuse() const
+{
+	return _light->getDiffuse();
+}
+
+void LightComponent::setSpecular(float red, float green, float blue)
+{
+	_light->setSpecular(red, green, blue);
+}
+
+void LightComponent::setSpecular(const Colour& specular)
+{
+	_light->setSpecular(specular.red, specular.green, specular.blue);
+}
+
+const Colour& LightComponent::getSpecular() const
+{
+	return _light->getSpecular();
+}
+
+void LightComponent::setAttenuation(float range, float constant, float linear, float quadratic)
+{
+	_light->setAttenuation(range, constant, linear, quadratic);
+}
+
+const float LightComponent::getAttenuationRange() const
+{
+	return _light->getAttenuationRange();
+}
+
+const float LightComponent::getAttenuationConstant() const
+{
+	return _light->getAttenuationConstant();
+}
+
+const float LightComponent::getAttenuationLinear() const
+{
+	return _light->getAttenuationLinear();
+}
+
+const float LightComponent::getAttenuationQuadric() const
+{
+	return _light->getAttenuationQuadric();
+}
+
+void LightComponent::setSpotlightRange(float innerAngle, float outerAngle, float falloff)
+{
+	_light->setSpotlightRange(innerAngle, outerAngle, falloff);
+}
+
+const float LightComponent::getSpotlightFallOff() const
+{
+	return _light->getSpotlightFallOff();
+}
+
+const float LightComponent::getSpotlightInnerAngle() const
+{
+	return _light->getSpotlightInnerAngle();
+}
+
+const float LightComponent::getSpotlightOuterAngle() const
+{
+	return _light->getSpotlightOuterAngle();
+}
+
+void LightComponent::setPowerScale(float power)
+{
+	_light->setPowerScale(power);
+}
+
+const float LightComponent::getPowerScale() const
+{
+	return _light->getPowerScale();
+}
+
+void LightComponent::setVisible(bool visible)
+{
+	_light->setVisible(visible);
+}
+
+const bool LightComponent::getVisible() const
+{
+	return _light->getVisible();
+}
+
+const LightComponent::LightType LightComponent::convertLightType(std::string type)
+{
+	if (type == "POINT") return LightComponent::LightType::POINT;
+	else if (type == "DIRECTIONAL") return LightComponent::LightType::DIRECTIONAL;
+	else if (type == "SPOTLIGHT") return LightComponent::LightType::SPOTLIGHT;
 	//else throw UknownLightTypeException
 }
 
--- Src/MotorUnitario/LightComponent.h
+++ Src/MotorUnitario/LightComponent.h
@@ -3,13 +3,24 @@
 #define LIGHT_COMPONENT_H
 
 #include "Component.h"
-#include "MotorGrafico/Light.h"
+#include "Colour.h"
 
 class Transform;
 class GameObject;
+class Light;
 
 class LightComponent : public Component
 {
+public:
+	enum class LightType {
+		/// Point light sources give off light equally in all directions, so require only position not direction
+		POINT = 0,
+		/// Directional lights simulate parallel light beams from a distant source, hence have direction but no position
+		DIRECTIONAL = 1,
+		/// Spotlights simulate a cone of light from a source so require position and direction, plus extra values for falloff
+		SPOTLIGHT = 2
+	};
+
 public:
 
 	LightComponent();
@@ -35,13 +46,13 @@ public:
 	/// Changes light type to given type
 	/// </summary>
 	/// <param name="type">: light's type</param>
-	inline void setType(Light::LightType type) { _light->setLightType(type); }
+	void setType(LightComponent::LightType type);
 
 	/// <summary>
 	/// Return light's type
 	/// </summary>
 	/// <returns>Light's type</returns>
-	inline Light::LightType getLightType() { return _light->getLightType(); }
+	LightComponent::LightType getLightType();
 
 	/// <summary>
 	/// Sets light's diffuse to given color
@@ -49,19 +60,19 @@ public:
 	/// <param name="red">: R parameter of the color</param>
 	/// <param name="green">: G parameter of the color</param>
 	/// <param name="blue">: B parameter of the color</param>
-	inline void setDiffuse(float red, float green, float blue) { _light->setDiffuse(red, green, blue); }
+	void setDiffuse(float red, float green, float blue);
 
 	/// <summary>
 	/// Sets light's diffuse to given color
 	/// </summary>
 	/// <param name="diffuse">: Colour structure</param>
-	inline void setDiffuse(const Colour& diffuse) { _light->setDiffuse(diffuse.red, diffuse.green, diffuse.blue); }
+	void setDiffuse(const Colour& diffuse);
 
 	/// <summary>
 	/// Returns light's diffuse color
 	/// </summary>
 	/// <returns>diffuse colour</returns>
-	inline const Colour& getDiffuse() const { _light->getDiffuse(); }
+	const Colour& getDiffuse() const;
 
 	/// <summary>
 	/// Sets light's specular to given color
@@ -69,19 +80,19 @@ public:
 	/// <param name="red">: R parameter of the color</param>
 	/// <param name="green">: G parameter of the color</param>
 	/// <param name="blue">: B parameter of the color</param>
-	inline void setSpecular(float red, float green, float blue) { _light->setSpecular(red, green, blue); }
+	void setSpecular(float red, float green, float blue);
 
 	/// <summary>
 	/// Sets light's diffuse to given color
 	/// </summary>
 	/// <param name="specular">: Colour structure</param>
-	inline void setSpecular(const Colour& specular) { _light->setSpecular(specular.red, specular.green, specular.blue); }
+	void setSpecular(const Colour& specular);
 
 	/// <summary>
 	/// Returns light's specular color
 	/// </summary>
 	/// <returns>specular colour</returns>
-	inline const Colour& getSpecular() const { _light->getSpecular(); }
+	const Colour& getSpecular() const;
 
 	/// <summary>
 	/// Sets attenuation for the light
@@ -90,31 +101,31 @@ public:
 	/// <param name="constant">: The constant factor in the attenuation formula: 1.0 means never attenuate, 0.0 is complete attenuation.</param>
 	/// <param name="linear">: The linear factor in the attenuation formula: 1 means attenuate evenly over the distance.</param>
 	/// <param name="quadratic">: The quadratic factor in the attenuation formula: adds a curvature to the attenuation formula.</param>
-	inline void setAttenuation(float range, float constant, float linear, float quadratic) { _light->setAttenuation(range, constant, linear, quadratic); }
+	void setAttenuation(float range, float constant, float linear, float quadratic);
 
 	/// <summary>
 	/// Returns light's attenuation range
 	/// </summary>
 	/// <returns>The absolute upper range of the light in world units</returns>
-	inline const float getAttenuationRange() const { return _light->getAttenuationRange(); }
+	const float getAttenuationRange() const;
 
 	/// <summary>
 	/// Returns the constant factor in the attenuation formula
 	/// </summary>
 	/// <returns>Constant factor of light's attenuation</returns>
-	inline const float getAttenuationConstant() const { return _light->getAttenuationConstant(); }
+	const float getAttenuationConstant() const;
 
 	/// <summary>
 	/// Returns the linear factor in the attenuation formula
 	/// </summary>
 	/// <returns>Linear factor of light's attenuation</returns>
-	inline const float getAttenuationLinear() const { return _light->getAttenuationLinear(); }
+	const float getAttenuationLinear() const;
 
 	/// <summary>
 	/// Returns the quadric factor in the attenuation formula
 	/// </summary>
 	/// <returns>quadric factor of light's attenuation</returns>
-	inline const float getAttenuationQuadric() const { return _light->getAttenuationQuadric(); }
+	const float getAttenuationQuadric() const;
 
 	/// <summary>
 	/// Only usefull if the type of this light is set to SpotLight
@@ -123,55 +134,55 @@ public:
 	/// <param name="outerAngle">: Angle covered by the outer cone.</param>
 	/// <param name="fallof">:  The rate of falloff between the inner and outer cones. 1.0 means a linear falloff,
 	///							less means slower falloff, higher means faster falloff.</param>
-	inline void setSpotlightRange(float innerAngle, float outerAngle, float falloff) { _light->setSpotlightRange(innerAngle, outerAngle, falloff); }
+	void setSpotlightRange(float innerAngle, float outerAngle, float falloff);
 
 	/// <summary>
 	/// Only usefull if light's type is set to spotLight
 	/// </summary>
 	/// <returns>splotlight fallOf</returns>
-	inline const float getSpotlightFallOff() const { return _light->getSpotlightFallOff(); }
+	const float getSpotlightFallOff() const;
 
 	/// <summary>
 	/// Only usefull if light's type is set to spotLight
 	/// </summary>
 	/// <returns>splotlight inner angle</returns>
-	inline const float getSpotlightInnerAngle() const { return _light->getSpotlightInnerAngle(); }
+	const float getSpotlightInnerAngle() const;
 
 	/// <summary>
 	/// Only usefull if light's type is set to spotLight
 	/// </summary>
 	/// <returns>splotlight outter angle</returns>
-	inline const float getSpotlightOuterAngle() const { return _light->getSpotlightOuterAngle(); }
+	const float getSpotlightOuterAngle() const;
 
 	/// <summary>
 	/// Sets the intensity of this light
 	/// </summary>
 	/// <param name="power">: Default value is already set at 1</param>
-	inline void setPowerScale(float power) { _light->setPowerScale(power); }
+	void setPowerScale(float power);
 
 	/// <summary>
 	/// Return the intensity of this light
 	/// </summary>
 	/// <returns>intensity of this light</returns>
-	inline const float getPowerScale() const { _light->getPowerScale(); }
+	const float getPowerScale() const;
 
 	/// <summary>
 	/// Changes light visibility
 	/// </summary>
 	/// <param name="visible">true means visible</param>
-	inline void setVisible(bool visible) { _light->setVisible(visible); }
+	void setVisible(bool visible);
 
 	/// <summary>
 	/// Returns the value set by MovableObject::setVisible only.
 	/// </summary>
 	/// <returns>if it's visible or not</returns>
-	inline const bool getVisible() const { return _light->getVisible(); }
+	const bool getVisible() const;
 
 	/// <summary>
 	///	String to light type converter required for lua.
 	/// </summary>
 	/// <returns>if it's visible or not</returns>
-	const Light::LightType convertLightType(std::string type);
+	const LightComponent::LightType convertLightType(std::string type);
 
 
 private:
